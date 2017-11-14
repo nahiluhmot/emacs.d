d147d8899676fd8b762d050d1ffc282a1ee9c476
Tom Hulihan <hulihan.tom159@gmail.com>
2017-11-14 11:17:37 -0500

    Fix C-u in evil-mode

--- init/init-evil.el
+++ init/init-evil.el
@@ -7,9 +7,9 @@
   :bind (:map evil-insert-state-map
               ("C-n" . evil-normal-state))
   :init
-  (evil-mode 1)
   (setq evil-want-C-u-scroll t)
   (setq evil-want-C-d-scroll t)
+  (evil-mode 1)
   :config
   (evil-set-initial-state 'dired-mode 'emacs)
   (evil-set-initial-state 'magit-mode 'emacs)
