ac661b5b4caef0df9ad0cd87e9a165d3f9397d98
Tom Hulihan <hulihan.tom159@gmail.com>
2017-11-14 12:25:12 -0500

    Force rubocop to do as I say

--- init/init-ruby.el
+++ init/init-ruby.el
@@ -1,11 +1,16 @@
 ;;; init-ruby.el
 
+(defun my-ruby-mode-hook ()
+  "Called when initializing ruby."
+  (setenv "RBENV_VERSION" "2.4.1"))
+
 (use-package enh-ruby-mode
   :ensure t
   :mode "\\.rb$"
   :interpreter "ruby"
   :config
   (setq ruby-deep-indent-paren nil)
-  (add-hook 'ruby-mode-hook 'rainbow-delimiters-mode))
+  (add-hook 'enh-ruby-mode-hook 'my-ruby-mode-hook)
+  (add-hook 'enh-ruby-mode-hook 'rainbow-delimiters-mode))
 
 (provide 'init-ruby)
