f89ee12d8ed0ce4393214b5372f49190ed647ba3
Tom Hulihan <hulihan.tom159@gmail.com>
2017-11-17 12:34:37 -0500

    Plain old ruby mode works for me

--- init/init-ruby.el
+++ init/init-ruby.el
@@ -4,13 +4,7 @@
   "Called when initializing ruby."
   (setenv "RBENV_VERSION" "2.4.1"))
 
-(use-package enh-ruby-mode
-  :ensure t
-  :mode "\\.rb$"
-  :interpreter "ruby"
-  :config
-  (setq ruby-deep-indent-paren nil)
-  (add-hook 'enh-ruby-mode-hook 'my-ruby-mode-hook)
-  (add-hook 'enh-ruby-mode-hook 'rainbow-delimiters-mode))
+(add-hook 'ruby-mode-hook 'my-ruby-mode-hook)
+(add-hook 'ruby-mode-hook 'rainbow-delimiters-mode)
 
 (provide 'init-ruby)
