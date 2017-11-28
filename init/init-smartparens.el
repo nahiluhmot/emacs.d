14626954d0d792093940ecf2650399b725733182
Tom Hulihan <hulihan.tom159@gmail.com>
2017-11-28 10:54:55 -0500

    Add keybinding for rewrapping sexps

--- init/init-smartparens.el
+++ init/init-smartparens.el
@@ -4,6 +4,7 @@
   :ensure t
   :config
   (require 'smartparens-config)
+  (define-key smartparens-mode-map (kbd "C-c r") 'sp-rewrap-sexp)
   (smartparens-global-mode 1))
 
 (provide 'init-smartparens)
