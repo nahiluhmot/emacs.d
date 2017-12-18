805cc506a7a22e5db8a8ad6dc32d8acc9fddb424
Tom Hulihan <hulihan.tom159@gmail.com>
2017-12-18 09:35:18 -0500

    Connect to the vpn before jenkins

--- init/init-jenkins.el
+++ init/init-jenkins.el
@@ -2,14 +2,20 @@
 
 ;;; Code:
 
+(defun init-connect-to-vpn ()
+  "Connect to the VPN."
+  (shell-command "osascript -e 'tell application \"Viscosity\" to connect \"Upserve\"'"))
+
 (defun init-jenkins-build ()
   "Start a jenkins build for the current repo."
   (interactive)
+  (init-connect-to-vpn)
   (shell-command "jenkins-build"))
 
 (defun init-jenkins-show ()
   "Open jenkins for the current repo."
   (interactive)
+  (init-connect-to-vpn)
   (shell-command "jenkins-show"))
 
 (global-set-key (kbd "C-c j b") 'init-jenkins-build)
