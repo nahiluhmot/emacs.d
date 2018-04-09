4e009561b17c0d0c90ea5cafcb89dc449451a802
Tom Hulihan <hulihan.tom159@gmail.com>
2018-04-09 09:03:53 -0400

    Use prettier js

--- init/init-javascript.el
+++ init/init-javascript.el
@@ -5,12 +5,15 @@
   (setq js2-basic-offset 2)
   (setq js2-indent-level 2))
 
+(use-package prettier-js
+  :ensure t)
 
 (use-package js2-mode
   :ensure t
   :mode ("\\.js$")
   :config
   (add-hook 'js2-mode-hook 'init-js2-mode)
-  (add-hook 'ruby-mode-hook 'rainbow-delimiters-mode))
+  (add-hook 'js2-mode-hook 'rainbow-delimiters-mode)
+  (add-hook 'js2-mode-hook 'prettier-js-mode))
 
 (provide 'init-javascript)
