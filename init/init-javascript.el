bc9d2eb23f550b74fa9937abc90ebea3d282fbef
Tom Hulihan <hulihan.tom159@gmail.com>
2018-09-14 19:13:22 -0400

    Disable js2-mode for now

--- init/init-javascript.el
+++ init/init-javascript.el
@@ -5,15 +5,15 @@
   (setq js2-basic-offset 2)
   (setq js2-indent-level 2))
 
+;; (use-package js2-mode
+;;   :ensure t
+;;   :mode ("\\.js$")
+;;   :config
+;;   (add-hook 'js2-mode-hook 'rainbow-delimiters-mode)
+;;   (add-hook 'js2-mode-hook 'prettier-js-mode)
+;;   (add-hook 'js2-mode-hook 'init-js2-mode))
+
 (use-package prettier-js
   :ensure t)
-
-(use-package js2-mode
-  :ensure t
-  :mode ("\\.js$")
-  :config
-  (add-hook 'js2-mode-hook 'init-js2-mode)
-  (add-hook 'js2-mode-hook 'rainbow-delimiters-mode)
-  (add-hook 'js2-mode-hook 'prettier-js-mode))
 
 (provide 'init-javascript)
