b1f127077798ca2ccfeb5e932194b188e7a32269
Tom Hulihan <hulihan.tom159@gmail.com>
2019-05-20 11:32:00 -0400

    Enable good js config

--- init/init-javascript.el
+++ init/init-javascript.el
@@ -5,13 +5,13 @@
   (setq js2-basic-offset 2)
   (setq js2-indent-level 2))
 
-;; (use-package js2-mode
-;;   :ensure t
-;;   :mode ("\\.js$")
-;;   :config
-;;   (add-hook 'js2-mode-hook 'rainbow-delimiters-mode)
-;;   (add-hook 'js2-mode-hook 'prettier-js-mode)
-;;   (add-hook 'js2-mode-hook 'init-js2-mode))
+(use-package js2-mode
+  :ensure t
+  :mode ("\\.js$")
+  :config
+  (add-hook 'js2-mode-hook 'rainbow-delimiters-mode)
+  (add-hook 'js2-mode-hook 'prettier-js-mode)
+  (add-hook 'js2-mode-hook 'init-js2-mode))
 
 (use-package prettier-js
   :ensure t)
