74206bbadabf69df0456a9fa279eb3afb54ee64b
Tom Hulihan <hulihan.tom159@gmail.com>
2019-05-24 08:55:45 -0400

    Cleanup tmux

--- init/init-tmux.el
+++ init/init-tmux.el
@@ -28,10 +28,10 @@
 
 (defun init-tmux-session-format (tmux-session)
   "Return a formatted string for the TMUX-SESSION."
-  (let ((name (init-tmux-session-name tmux-session)))
-    (if (init-tmux-session-attached tmux-session)
-        (concat "* " name)
-      (concat "  " name))))
+  (let* ((name (init-tmux-session-name tmux-session))
+         (attached (init-tmux-session-attached tmux-session))
+         (prefix (if attached "* " "  ")))
+    (concat prefix name)))
 
 ; tmux-client
 
@@ -57,7 +57,6 @@
 
 (defun init-tmux-shell-command (&rest args)
   "Run the tmux executable with ARGS.  If the command exits zero, return `stdout' and `stderr'.  Otherwise, raise an error."
-  ; random buffer name should be good enough to be unique
   (let* ((buffer-name (concat "*init-tmux-"
                               (number-to-string (round (time-to-seconds (current-time))))
                               "-"
@@ -77,7 +76,7 @@
    (lambda (line)
      (let ((name (car (s-split-up-to ":" line 1)))
            (attached (s-ends-with? "(attached)" line)))
-        (init-tmux-session-alloc name attached)))
+       (init-tmux-session-alloc name attached)))
    (mapcar 's-chomp (s-lines (s-chomp (init-tmux-shell-command "ls"))))))
 
 (defun init-tmux-list-clients ()
@@ -104,6 +103,37 @@
                            "-c" session-dir
                            "-d"))
 
+(defun init-tmux-find-or-create-session-for-client (client session-name session-dir)
+  "Switch CLIENT to SESSION-NAME, creating a new session in SESSION-DIR if id doesn't already exist."
+  (let* ((sessions (init-tmux-list-sessions))
+         (session-names (mapcar 'init-tmux-session-name sessions))
+         (tty (init-tmux-client-tty client)))
+    (unless (seq-contains session-names session-name)
+      (init-tmux-new-session session-name session-dir))
+    (init-tmux-switch-client tty session-name)))
+
+(defun init-tmux-choose-disconnected-client (clients session-name)
+  "Chose a client from CLIENTS which is not connected to SESSION-NAME, prompting for user input if needed."
+  (let* ((matching-session-name? (lambda (client)
+                                   (string= session-name
+                                            (init-tmux-client-session-name client))))
+         (possible-clients (seq-remove matching-session-name? clients)))
+    (cond ((= (length clients) 0)
+           (error "No ttys are connected to tmux"))
+          ((= (length possible-clients) 0)
+           (message "All ttys are connected to %s" session-name)
+           nil)
+          ((= (length possible-clients) 1)
+           (car possible-clients))
+          (t
+           (let* ((prompt (format "Please select a tty to connect to %s: " session-name))
+                  (options (mapcar 'init-tmux-client-tty possible-clients))
+                  (selected-tty (completing-read prompt options nil t))
+                  (matching-tty? (lambda (client)
+                                   (string= selected-tty
+                                            (init-tmux-client-tty client)))))
+             (seq-find matching-tty? possible-clients))))))
+
 ;; UI
 
 (defun init-tmux-ls ()
@@ -123,28 +153,20 @@
 (defun init-tmux-focus-current-project ()
   "Focus the current projectile project in tmux."
   (interactive)
-  (let* ((clients (init-tmux-list-clients))
-         (len (length clients)))
-    (cond ((eq len 0) (error "No ttys are connected to tmux"))
-          ((> len 1) (error "Too many ttys connected to tmux"))
-          ((= len 1)
-           (let* ((client (car clients))
-                  (tty (init-tmux-client-tty client))
-                  (session-name (init-tmux-client-session-name client))
-                  (sessions (init-tmux-list-sessions))
-                  (project-name (s-replace-all '((" " . "-") ("." . "-")) (projectile-project-name)))
-                  (project-root (s-chop-suffix "/" (projectile-project-root))))
-             (cond ((eq session-name project-name)
-                    (message "%s is already connected to %s" tty session-name))
-                   ((seq-contains (mapcar 'init-tmux-session-name sessions) project-name)
-                    (init-tmux-switch-client tty project-name)
-                    (message "Switched %s to %s" tty project-name))
-                   (t
-                    (init-tmux-new-session project-name project-root)
-                    (init-tmux-switch-client tty project-name)
-                    (message "Created and switched %s to %s" tty project-name))))))))
+  (let* ((session-name (s-replace-all '((" " . "-") ("." . "-"))
+                                      (projectile-project-name)))
+         (session-dir (s-chop-suffix "/"
+                                     (projectile-project-root)))
+         (clients (init-tmux-list-clients))
+         (chosen-client (init-tmux-choose-disconnected-client clients session-name)))
+    (when chosen-client
+      (init-tmux-find-or-create-session-for-client chosen-client
+                                                   session-name
+                                                   session-dir)
+      (message "Switched %s to %s" (init-tmux-client-tty chosen-client) session-name))))
 
-(global-set-key (kbd "C-c p x m") 'init-tmux-focus-current-project)
+(global-set-key (kbd "C-c p .") 'init-tmux-focus-current-project)
+(add-hook 'projectile-after-switch-project-hook 'init-tmux-focus-current-project)
 
 (provide 'init-tmux)
 ;;; init-tmux ends here
