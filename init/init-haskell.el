4d0e6465adde7dbf0c17ad5cf92aa57b00db874e
Tom Hulihan <hulihan.tom159@gmail.com>
2019-05-30 17:49:18 -0400

    More rainbow delimiters

--- init/init-haskell.el
+++ init/init-haskell.el
@@ -1,7 +1,9 @@
 ;;; init-haskell.el
 
 (use-package haskell-mode
-  :ensure t)
+  :ensure t
+  :init
+  (add-hook 'haskell-mode-hook 'rainbow-delimiters-mode))
 
 (custom-set-variables
  '(haskell-stylish-on-save t))
--- init/init-typescript.el
+++ init/init-typescript.el
@@ -11,6 +11,7 @@
   :init
   (setq tide-format-options '(:insertSpaceAfterFunctionKeywordForAnonymousFunctions t :placeOpenBraceOnNewLineForFunctions nil :tabSize 2 :indentSize 2))
   (setq typescript-indent-level 2)
-  (add-hook 'typescript-mode-hook 'init-tide-mode))
+  (add-hook 'typescript-mode-hook 'init-tide-mode)
+  (add-hook 'typescript-moode-hook 'rainbow-delimiters-mode))
 
 (provide 'init-typescript)
