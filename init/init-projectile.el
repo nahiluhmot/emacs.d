0982f3ce48a54ecba69c8c499653c61416870a8a
Tom Hulihan <hulihan.tom159@gmail.com>
2019-06-30 22:31:47 -0400

    New projectile who dis

--- init/init-projectile.el
+++ init/init-projectile.el
@@ -3,10 +3,11 @@
 (use-package projectile
   :ensure t
   :init
-  (setq projectile-keymap-prefix (kbd "C-c p"))
   :config
   (setvar projectile-indexing-mode 'alien)
+  (define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map)
   (projectile-global-mode))
+
 
 (use-package helm-projectile
   :ensure t
