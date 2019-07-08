93e873e32321dcafd43962548614017d0a57a046
Tom Hulihan <hulihan.tom159@gmail.com>
2019-07-08 13:19:35 -0400

    More haskell

--- init/init-haskell.el
+++ init/init-haskell.el
@@ -1,9 +1,25 @@
 ;;; init-haskell.el
 
+(use-package lsp-mode
+  :ensure t
+  :init
+  (setq lsp-enable-snippet nil))
+
+(use-package lsp-ui
+  :ensure t
+  :init
+  (add-hook 'lsp-mode-hook 'lsp-ui-mode))
+
+(use-package lsp-haskell
+  :ensure t)
+
 (use-package haskell-mode
   :ensure t
   :init
-  (add-hook 'haskell-mode-hook 'rainbow-delimiters-mode))
+  (setq lsp-haskell-process-path-hie "hie-wrapper")
+  (setq lsp-prefer-flymake nil)
+  (add-hook 'haskell-mode-hook 'rainbow-delimiters-mode)
+  (add-hook 'haskell-mode-hook #'lsp))
 
 (custom-set-variables
  '(haskell-stylish-on-save t))
