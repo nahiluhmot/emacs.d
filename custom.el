c7316c44ad6051a8e6f07bc066f18c06faceb24c
Tom Hulihan <hulihan.tom159@gmail.com>
2019-07-08 13:18:04 -0400

    More theme hashes?

--- custom.el
+++ custom.el
@@ -7,7 +7,7 @@
    ["#1d1f21" "#cc6666" "#b5bd68" "#f0c674" "#81a2be" "#c9b4cf" "#8abeb7" "#c5c8c6"])
  '(custom-safe-themes
    (quote
-    ("6b289bab28a7e511f9c54496be647dc60f5bd8f9917c9495978762b99d8c96a0" "8aca557e9a17174d8f847fb02870cb2bb67f3b6e808e46c0e54a44e3e18e1020" "6b2636879127bf6124ce541b1b2824800afc49c6ccd65439d6eb987dbf200c36" "fe666e5ac37c2dfcf80074e88b9252c71a22b6f5d2f566df9a7aa4f9bea55ef8" "356e5cbe0874b444263f3e1f9fffd4ae4c82c1b07fe085ba26e2a6d332db34dd" "a866134130e4393c0cad0b4f1a5b0dd580584d9cf921617eee3fd54b6f09ac37" "fa2b58bb98b62c3b8cf3b6f02f058ef7827a8e497125de0254f56e373abee088" "bffa9739ce0752a37d9b1eee78fc00ba159748f50dc328af4be661484848e476" default)))
+    ("80365dd15f97396bdc38490390c23337063c8965c4556b8f50937e63b5e9a65c" "6b289bab28a7e511f9c54496be647dc60f5bd8f9917c9495978762b99d8c96a0" "8aca557e9a17174d8f847fb02870cb2bb67f3b6e808e46c0e54a44e3e18e1020" "6b2636879127bf6124ce541b1b2824800afc49c6ccd65439d6eb987dbf200c36" "fe666e5ac37c2dfcf80074e88b9252c71a22b6f5d2f566df9a7aa4f9bea55ef8" "356e5cbe0874b444263f3e1f9fffd4ae4c82c1b07fe085ba26e2a6d332db34dd" "a866134130e4393c0cad0b4f1a5b0dd580584d9cf921617eee3fd54b6f09ac37" "fa2b58bb98b62c3b8cf3b6f02f058ef7827a8e497125de0254f56e373abee088" "bffa9739ce0752a37d9b1eee78fc00ba159748f50dc328af4be661484848e476" default)))
  '(fci-rule-color "#5c5e5e")
  '(haskell-stylish-on-save t)
  '(jdee-db-active-breakpoint-face-colors (cons "#0d0d0d" "#41728e"))
@@ -16,7 +16,7 @@
  '(objed-cursor-color "#cc6666")
  '(package-selected-packages
    (quote
-    (0blayout company-go doom-themes haskell-mode spacemacs-theme afternoon-theme rbenv projectile-rails prettier-js go-mode js2-mode tide s csv-mode helm-ag exec-path-from-shell yaml-mode web-mode ujelly-theme smartparens rainbow-delimiters helm-projectile projectile markdown-mode magit helm flycheck evil-surround evil diff-hl company coffee-mode use-package)))
+    (lsp-haskell lsp-ui lsp-mode 0blayout company-go doom-themes haskell-mode spacemacs-theme afternoon-theme rbenv projectile-rails prettier-js go-mode js2-mode tide s csv-mode helm-ag exec-path-from-shell yaml-mode web-mode ujelly-theme smartparens rainbow-delimiters helm-projectile projectile markdown-mode magit helm flycheck evil-surround evil diff-hl company coffee-mode use-package)))
  '(safe-local-variable-values
    (quote
     ((flycheck-clang-include-path
