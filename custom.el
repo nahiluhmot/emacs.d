a8403a58f1977db225239fb76fc9ac32bccf4c72
Tom Hulihan <hulihan.tom159@gmail.com>
2023-08-03 14:30:38 -0400

    fix the font?

--- custom.el
+++ custom.el
@@ -65,4 +65,4 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(font-lock-type-face ((t (:foreground "#bb9200" :family "Source Code Pro")))))
+ '(font-lock-type-face ((t (:foreground "#bb9200" :family "Menlo")))))
