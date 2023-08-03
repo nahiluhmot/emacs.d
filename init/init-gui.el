4417537b31ad19b8b2e0b8256872a00fe5041a3b
Tom Hulihan <hulihan.tom159@gmail.com>
2023-08-03 10:24:26 -0400

    old man eyes

--- init/init-gui.el
+++ init/init-gui.el
@@ -8,7 +8,7 @@
 
 (if (display-graphic-p)
     (progn
-      (set-face-attribute 'default nil :height 160)
+      (set-face-attribute 'default nil :height 180)
       (use-package doom-themes
         :ensure t)
       (load-theme 'doom-tomorrow-day))
